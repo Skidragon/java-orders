2a1e8fcd90442e1e71b6d3917c929d5f6d97aa36
Simon Davis <simon_freelance@hotmail.com>
2019-02-19 16:43:20 -0500

    Order class created with relationships setup.

---
+++ src/main/java/com/lambdaschool/orders/models/Order.java
@@ -0,0 +1,67 @@
+package com.lambdaschool.orders.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="order")
+public class Order {
+    @Id
+    @Column(nullable = false)
+    private long ordnum;
+
+    private double ordamount;
+
+    private double advanceamount;
+
+    @ManyToOne
+    @JoinColumn(name="custcode", nullable=false)
+    @JsonIgnore
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name="agentcode", nullable = false)
+    @JsonIgnore
+    private Agent agent;
+
+
+    public Order() {
+    }
+
+    public long getOrdnum() {
+        return ordnum;
+    }
+
+    public double getOrdamount() {
+        return ordamount;
+    }
+
+    public void setOrdamount(double ordamount) {
+        this.ordamount = ordamount;
+    }
+
+    public double getAdvanceamount() {
+        return advanceamount;
+    }
+
+    public void setAdvanceamount(double advanceamount) {
+        this.advanceamount = advanceamount;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public Agent getAgent() {
+        return agent;
+    }
+
+    public void setAgent(Agent agent) {
+        this.agent = agent;
+    }
+}
